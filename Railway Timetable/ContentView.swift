e32a6a0ce52fdb1855d6f486d1f7edc51dd50193
dnyBLUESKY007 <302901580@qq.com>
2023-07-22 20:52:45 +0800

    Initial Commit

---
+++ Railway Timetable/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Railway Timetable
+//
+//  Created by BLUESKY007 on 2023-07-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Railway Timetable/Railway_TimetableApp.swift
@@ -0,0 +1,17 @@
+//
+//  Railway_TimetableApp.swift
+//  Railway Timetable
+//
+//  Created by BLUESKY007 on 2023-07-22.
+//
+
+import SwiftUI
+
+@main
+struct Railway_TimetableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
